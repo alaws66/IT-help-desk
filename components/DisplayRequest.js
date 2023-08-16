6111de10045423b976616b755ae36e0b85022121
Aimee Laws <aimee.laws13@gmail.com>
2023-08-16 12:07:24 +0100

    update status

--- components/DisplayRequest.js
+++ components/DisplayRequest.js
@@ -2,16 +2,51 @@
 
 import formatDate from '@/utils/formatDate';
 import {
+  Alert,
+  Button,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
   TextField
 } from '@mui/material';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const DisplayRequest = ({ request }) => {
+  const router = useRouter();
+
   const [status, setStatus] = useState(request.status);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(false);
+
+  const handleSave = async () => {
+    setLoading(true);
+
+    // update status
+    const response = await fetch('http://localhost:3000/api/update-request', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          id: request._id,
+          status
+        }
+      )
+    });
+
+    if (response.status === 200) {
+      router.refresh();
+      router.push('/requests');
+    } else {
+      setAlert(true);
+    }
+
+    setLoading(false);
+  }
 
   return (
     <div className="flex flex-col gap-3 px-3 sm:px-5 w-full lg:w-3/4 2xl:w-1/2">
@@ -84,6 +119,47 @@ const DisplayRequest = ({ request }) => {
         minRows={5}
         className="w-full"
       />
+
+      {/* form action buttons */}
+      <div className="flex flex-col sm:flex-row justify-between gap-3">
+        <Button
+          disabled={loading}
+          variant="contained"
+          color="error"
+          className="order-1"
+        >
+          Delete Request
+        </Button>
+
+        <div className="flex gap-3 sm:order-2">
+          <Button
+            disabled={loading}
+            variant="outlined"
+            className="w-full sm:w-auto"
+            onClick={() => router.push('/requests')}
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={loading}
+            variant="contained"
+            className="w-full sm:w-auto"
+            onClick={handleSave}
+          >
+            Save
+          </Button>
+        </div>
+      </div>
+
+      {/* error alert */}
+      {alert &&
+        <Alert
+          severity="error"
+          onClose={() => setAlert(false)}
+        >
+          Error - Unable to update request
+        </Alert>
+      }
     </div>
   );
 }
