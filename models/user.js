a5b5ee2e6d46d461716ddca9ef0cd79684de3c53
Aimee Laws <aimee.laws13@gmail.com>
2023-08-16 11:01:02 +0100

    userSchema typo fix

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 import { Schema, model, models } from 'mongoose';
 
 const userSchema = new Schema({
-  firstname: String,
-  lastname: String,
+  firstName: String,
+  lastName: String,
   username: String,
   password: { type: String, select: false },
   loggedIn: Boolean
